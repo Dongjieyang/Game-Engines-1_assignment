d4eb15d89bb71bed5b1d71deea91d83f3a0c8781
Dongjieyang <56842526+Dongjieyang@users.noreply.github.com>
2021-12-10 23:58:45 +0000

    shape

--- Universe/Assets/Scripts/AsteroidSpawn.cs
+++ Universe/Assets/Scripts/AsteroidSpawn.cs
@@ -6,18 +6,21 @@ public class AsteroidSpawn : MonoBehaviour
 {
     public int density;
     public float innerR,outerR,height;
-    private Vector3 localPosition, offset, worldPosition;
+    private Vector3 localPosition, offset, worldPosition, scaleChange;
     private float randomR, randomRandian, x, y, z;
     private float xRot, yRot, zRot;
     private GameObject asteroids;
 
+    public float sphereR;
+
 
     void Start()
     {
+        
 
         for (int i = 0; i < density; i++)
         {
-            asteroids = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Shape();
 
             randomR = Random.Range(innerR, outerR);
             randomRandian = Random.Range(0, (2 * Mathf.PI));
@@ -25,7 +28,6 @@ public class AsteroidSpawn : MonoBehaviour
             x = randomR * Mathf.Cos(randomRandian);
             y = Random.Range(-height, height);
             z = randomR * Mathf.Sin(randomRandian);
-
 
 
             localPosition = new Vector3(x, y, z);
@@ -38,9 +40,17 @@ public class AsteroidSpawn : MonoBehaviour
 
             asteroids.transform.position = worldPosition;
             asteroids.transform.rotation = Quaternion.Euler(xRot, yRot, zRot);
-
             asteroids.transform.parent = this.transform;
         }
         
+    }
+
+    void Shape()
+    {
+        asteroids = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        scaleChange = new Vector3(Random.Range(0, 5), Random.Range(0, 5), Random.Range(0, 5));
+        asteroids.transform.localScale = scaleChange;
+       
     }
 }
